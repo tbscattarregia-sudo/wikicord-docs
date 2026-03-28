e2d2ebbfcd87a79015767c030000ce157c70d661
GameCodeYT <scattarregiatiziano@gmail.com>
2026-03-28 22:59:21 +0100

    init docs mejora visual

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 const content = document.getElementById("page-content");
+const links = document.querySelectorAll("nav a");
 
 async function loadPage(page) {
   try {
     const res = await fetch(`./pages/${page}.html`);
     const html = await res.text();
     content.innerHTML = html;
-  } catch (err) {
-    content.innerHTML = "<h1>Error cargando página</h1>";
+  } catch {
+    content.innerHTML = "<h1>404 - Página no encontrada</h1>";
   }
 }
 
-document.querySelectorAll("nav a").forEach(link => {
-  link.addEventListener("click", e => {
-    e.preventDefault();
-    const page = link.dataset.page;
-    loadPage(page);
+function setActiveLink(hash) {
+  links.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === hash) {
+      link.classList.add("active");
+    }
   });
-});
+}
 
-loadPage("overview");
+function router() {
+  let hash = window.location.hash;
+
+  if (!hash) {
+    hash = "#/overview";
+    window.location.hash = hash;
+  }
+
+  const page = hash.replace("#/", "");
+
+  loadPage(page);
+  setActiveLink(hash);
+}
+
+window.addEventListener("hashchange", router);
+window.addEventListener("load", router);
